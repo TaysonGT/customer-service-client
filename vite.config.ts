d8949f6970ef0ecd3beabbdbba64b5b8c1ee543d
Gomaa <mohamedmanno2003@gmail.com>
2025-08-31 15:12:54 +0300

    feat(auth): streamline API configuration and error handling
    Simplify vite proxy configuration to development-only setup
    
    Implement environment-aware base URL in axios instance

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
-export default defineConfig (({mode})=>{
-  const env = loadEnv(mode, process.cwd(), '')
-  return{
-    plugins: [tailwindcss()],
-    // define: {
-    //   "process.env": env,
-    // },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.NODE_ENV === 'production'? env.VITE_BACKEND_URL: 'http://localhost:5000',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
-        }
+export default defineConfig ({
+  plugins: [tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
